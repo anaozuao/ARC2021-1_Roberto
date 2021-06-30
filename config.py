5a9983517f1c7eb1112cda50579d1af8659a4b32
anaozuao <robertoandrade.jr@hotmail.com>
2021-06-30 05:55:59 +0000

    Criar o ambiante do errbot

---
+++ config.py
@@ -0,0 +1,16 @@
+import logging
+
+# This is a minimal configuration to get you started with the Text mode.
+# If you want to connect Errbot to chat services, checkout
+# the options in the more complete config-template.py from here:
+# https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
+
+BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+
+BOT_DATA_DIR = r'/workspace/ARC2021-1_Roberto/data'
+BOT_EXTRA_PLUGIN_DIR = r'/workspace/ARC2021-1_Roberto/plugins'
+
+BOT_LOG_FILE = r'/workspace/ARC2021-1_Roberto/errbot.log'
+BOT_LOG_LEVEL = logging.DEBUG
+
+BOT_ADMINS = ('@CHANGE_ME', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!

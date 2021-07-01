e28a41f4148e8f4ba372d2b95cb1465b9b24ce32
anaozuao <robertoandrade.jr@hotmail.com>
2021-07-01 04:10:02 +0000

    Mudanças feitas na aula 16/06

--- plugins/err-example/example.py
+++ plugins/err-example/example.py
@@ -3,16 +3,14 @@ from errbot import BotPlugin, botcmd
 
 class Example(BotPlugin):
     """
-    This is a very basic plugin to try out your new installation and get you started.
-    Feel free to tweak me to experiment with Errbot.
-    You can find me in your init directory in the subdirectory plugins.
+    apenas uma classe de métodos para aprendizado.
     """
 
     @botcmd  # flags a command
-    def tryme(self, msg, args):  # a command callable with !tryme
+    def bomdia(self, msg, args):
         """
-        Execute to check if Errbot responds to command.
-        Feel free to tweak me to experiment with Errbot.
-        You can find me in your init directory in the subdirectory plugins.
+        Resposnde educadamente ao usuário
         """
-        return "It *works* !"  # This string format is markdown.
+        
+        argumentos = args.split(' ')
+        return "Bom dia quinta-feira"
